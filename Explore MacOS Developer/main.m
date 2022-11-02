a3d2e2c58e112581726eb27ee7f77edd75a01553
zheng-anbin <2205623938@qq.com>
2022-11-02 15:39:38 +0800

    Initial Commit

---
+++ Explore MacOS Developer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Explore MacOS Developer
+//
+//  Created by macos on 2022/11/2.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
